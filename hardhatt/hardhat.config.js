cd5daabb91deaf6c888de63873e1fd2a461d028f
Manisha Jain <manishajain0607@gmail.com>
2022-09-26 21:47:51 +0530

    Final Commit

--- hardhatt/hardhat.config.js
+++ hardhatt/hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const {QUICKNODE_HTTP_URL,
+PRIVATE_KEY,
+POLYGONSCAN_KEY} = process.env
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    mumbai:{
+      url:QUICKNODE_HTTP_URL,
+      accounts:[PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey:{
+      polygonMumbai: POLYGONSCAN_KEY
+    }
+  }
 };
--- hardhatt/scripts/deploy.js
+++ hardhatt/scripts/deploy.js
@@ -1,31 +1,39 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const {ethers} = require("hardhat");
+require("dotenv").config({path:".env"});
+const { FEE, VRF_COORDINATOR, LINK_TOKEN, KEY_HASH } = require("../constants");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  
+  const randomWinnerGame = await ethers.getContractFactory("RandomWinnerGame");
+  const deployedRandomWinnerGame = await randomWinnerGame.deploy(
+    VRF_COORDINATOR,
+    LINK_TOKEN,
+    KEY_HASH,
+    FEE
+    );
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
+  await deployedRandomWinnerGame.deployed();
 
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    "Random Winner Game Contarct Address:", deployedRandomWinnerGame.address
   );
+
+  console.log("Sleeping...");
+  await sleep(30000);
+
+  await hre.run("verify:verify", {
+    address: deployedRandomWinnerGame.address,
+    constructorArguments: [VRF_COORDINATOR, LINK_TOKEN, KEY_HASH, FEE]
+  })
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
+function sleep(ms){
+  return new Promise((resolve) => setTimeout(resolve,ms));
+}
+
+main()
+.then(() => ProcessingInstruction.exit(0))
+.catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
